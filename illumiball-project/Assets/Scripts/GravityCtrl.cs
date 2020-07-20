87775873c60737ac2636062633b4dd0e8a7eaae9
shinaaaaa <51100609+shinaaaaa@users.noreply.github.com>
2020-07-21 05:33:28 +0900

    alpha1

---
+++ illumiball-project/Assets/Scripts/GravityCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCtrl : MonoBehaviour {
+    const float gravity = 9.8f; //gravity acceleration
+    public float gravityScale = 1.0f; //gravity accelertion scale
+
+    void Update () {
+        Vector3 vector = new Vector3 ();
+        if(Application.isMobilePlatform){
+            vector.x = Input.acceleration.x;
+            vector.y = Input.acceleration.y;
+            vector.z = Input.acceleration.z;
+        }
+        //detect key ->set vector
+        vector.x = Input.GetAxis("Horizontal");
+        vector.y = Input.GetAxis("Vertical");
+
+        //hight->Z key
+        if(Input.GetKey("z")){
+            vector.y = 1.0f;
+        }else{
+            vector.y = -1.0f;
+        }
+        //change scene gravity,input vector
+        Debug.Log(gravity * vector.normalized * gravityScale);
+        Physics.gravity = gravity * vector.normalized * gravityScale;
+    }
+
+    void FixedUpdate () {
+
+    }
+
+}
